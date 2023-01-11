9de6a3cea274b37b39ab7989e685afcf84885fa0
Amit Asher <asheramit4@gmail.com>
2023-01-12 01:04:14 +0200

    initial commit

---
+++ example/code.ts
@@ -0,0 +1,10 @@
+import { Routes } from "./output";
+
+console.log(Routes.path);
+console.log(Routes.Nba.News.path);
+console.log(Routes.Nba.Players.path);
+console.log(Routes.Nba.Scoreboard.path);
+console.log(Routes.Golf.path);
+console.log(Routes.Soccer.path);
+console.log(Routes.Sportsbook.Experts.path);
+console.log(Routes.Sportsbook.News.path);
